3fb230a678a7ad323851c75153e6ad0c1e4d09ce
AsyncRifat <ibrahim3rifat@gmail.com>
2025-09-05 17:43:50 +0600

    new branch created for rifat

---
+++ src/components/shared/Typography/Text.jsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import React from 'react';
+import ReactHtmlParser from 'html-react-parser';
+
+const Text = ({ tag, text, className, href = '#', ...rest }) => {
+  const getTag = tag => {
+    switch (tag) {
+      case 'h1':
+        return (
+          <h1
+            className={cn(
+              'font-bold capitalize lg:text-3xl text-2xl',
+              className
+            )}
+            {...rest}
+          >
+            {ReactHtmlParser(text || '')}
+          </h1>
+        );
+      case 'heading':
+        return (
+          <h2
+            className={cn(
+              'font-bold capitalize text-[50px] text-red-400',
+              className
+            )}
+            {...rest}
+          >
+            {ReactHtmlParser(text || '')}
+          </h2>
+        );
+      case 'small':
+        return (
+          <span
+            className={cn('text-[14px] text-white pl-1 leading-6', className)}
+            {...rest}
+          >
+            {ReactHtmlParser(text || '')}
+          </span>
+        );
+      case 'terms':
+        return (
+          <span
+            className={cn(
+              'block text-base sm:text-lg w-[100%] lg:w-[70%] text-stone-950 leading-6',
+              className
+            )}
+            {...rest}
+          >
+            {ReactHtmlParser(text || '')}
+          </span>
+        );
+      case 'link':
+        return (
+          <Link
+            href={href}
+            className={cn(
+              'text-white transition-all duration-300 ease-linear leading-9 text-base font-normal capitalize hover:text-[#2eca7f]',
+              className
+            )}
+          >
+            {ReactHtmlParser(text || '')}
+          </Link>
+        );
+      case 'paragraph':
+      default:
+        return (
+          <div className={cn('text-[16px] text-[#888]', className)} {...rest}>
+            {ReactHtmlParser(text || '')}
+          </div>
+        );
+    }
+  };
+
+  return getTag(tag || 'paragraph');
+};
+
+export default Text;
