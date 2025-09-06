d8f907b51c069d482b9c9c857bec17d67773508b
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-09-06 23:45:42 +0600

    Folder structure change: added questions to dashboard from homelayout and made dashboard navbar functional

--- src/components/nav-main.jsx
+++ src/components/nav-main.jsx
@@ -1,42 +1,44 @@
 "use client"
 
-import { ChevronRight } from "lucide-react";
-
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible"
 import {
   SidebarGroup,
-  SidebarGroupLabel,
+
   SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-} from "@/components/ui/sidebar"
 
-export function NavMain({
-  items
-}) {
+} from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export function NavMain({navDetails}) {
+  const pathname = usePathname();
   return (
     <SidebarGroup>
       {/* <SidebarGroupLabel>Platform</SidebarGroupLabel> */}
       <SidebarMenu>
-        {items.map((item) => (
+        {navDetails.map((item) => {
+          const isActive = pathname === item.url
 
-
-          <SidebarMenuButton key={item.title} tooltip={item.title}>
-            {item.icon && <item.icon />}
-            <span>{item.title}</span>
-
-          </SidebarMenuButton>
-
-
-
-        ))}
+          return (
+            <SidebarMenuButton
+              asChild
+              key={item.title}
+              tooltip={item.title}
+              className={cn(
+                "flex items-center gap-2 rounded-lg px-3 py-2 transition-colors",
+                isActive
+                  ? "bg-secondary text-secondary-foreground"
+                  : "hover:bg-muted hover:text-foreground"
+              )}
+            >
+              <Link href={item.url}>
+                {item.icon && <item.icon className="h-4 w-4" />}
+                <span>{item.title}</span>
+              </Link>
+            </SidebarMenuButton>
+          )
+        })}
       </SidebarMenu>
     </SidebarGroup>
   );
