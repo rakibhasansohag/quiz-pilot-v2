af4f594750dd8ada8dac7c9753dda1ceab9057a0
AsyncRifat <ibrahim3rifat@gmail.com>
2025-09-07 08:39:05 +0600

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,10 @@ import HeroSection from '@/components/HeroSection';
 import Marquee from '@/components/Marquee';
 import Navbar from '@/components/Navbar';
 import ResponsiveWidthProvider from '@/components/shared/ResponsiveWidthProvider/ResponsiveWidthProvider';
-import Text from '@/components/shared/Typography/Text';
 
 export default async function Home() {
   return (
-    <div>
+    <>
       <nav>
         <Navbar />
       </nav>
@@ -22,6 +21,6 @@ export default async function Home() {
       <footer className="mt-32">
         <Footer />
       </footer>
-    </div>
+    </>
   );
 }
